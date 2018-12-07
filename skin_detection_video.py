90494b8900204a37f1303168280fce64f16f45e7
daleyprabhakar <40574296+daleyprabhakar@users.noreply.github.com>
2018-12-07 15:44:23 +0530

    Update and rename skin-Detection.py to skin_detection_video.py

--- skin_detection_video.py
+++ skin_detection_video.py
@@ -9,18 +9,22 @@ import cv2
 import numpy as np
 cap=cv2.VideoCapture(0)
 while True:
+    #Capturing video frame by frame
     ret , frame=cap.read()
+    # Convert BGR image to HSV image format
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #hsv hue sat value
+    #hsv hue sat value metrics for filtering of the image
     lower = np.array([0, 48, 80])
     upper = np.array([20, 255, 255])
+    #Masking of image based on lower and upper limits
     mask = cv2.inRange(hsv, lower, upper)
+    # Taking the image of only filtered pixels
     res = cv2.bitwise_and(frame, frame, mask= mask)
-    #visualization
+    #visualizating the image
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     cv2.imshow('res',res)
-    
+    # Press ESC key to opt of the video
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
